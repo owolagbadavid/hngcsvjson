d1afaa8ee5b1068e1c006de2815859f599e509d6
David <owolagbadavid@gmail.com>
2022-11-04 15:31:51 +0100

    Files updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let csvToJson = require('convert-csv-to-json');
 let { createHash } =require('node:crypto')
 let {csvAppend} = require("csv-append");
-
+let fs = require('fs');
 
 function sha256(content) {  
   return createHash('sha256').update(content).digest('hex')
@@ -16,12 +16,39 @@ for(let i=0; i<json.length;i++){
 }
 
 //CALCUALTE AND ADD THE SHA256 OF ALL JSON FILES TO EACH JSON FILE
-array.map(item=>{
+array = array.map((item, index)=>{
+if(!item['TEAMNAMES'].length){
+    item['TEAMNAMES'] = array[index-1]['TEAMNAMES']
+}
+let attr = item['Attributes'].split(';')
+attr = attr.map(output=>output.split(':'))
+attr = attr.map(output=>{
+    return output.map(output2=>{
+            return output2.trim();
+    })
+})
+
+attr = Object.fromEntries(attr)
+    item['Attributes'] = attr
     item['hash'] = sha256(JSON.stringify(item))
     return item;
 })
 console.log(array)
+let jsonOutput = JSON.stringify(array)
+fs.writeFileSync('output.json', jsonOutput);
 
+
+let array2 = array.map(item=>{
+    let attr = item['Attributes']
+    attr = Object.entries(attr)
+    attr = attr.map(output=>output.join(':'))
+ 
+    return attr
+})
+array = array.map((item, index)=>{
+item['Attributes'] = array2[index].join(';')
+return item;
+})
 //CONVERT TO CSV WITH THE ADDED FIELDS
 const RELATIVE_PATH_TO_CSV = `output.csv`;
 const { append, end } = csvAppend(RELATIVE_PATH_TO_CSV);
